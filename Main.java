7191dca6b6659cb69bba979b78c1595cb19a2e9b
Gael González Méndez <gaelglz032@gmail.com>
2025-09-23 09:09:04 -0600

    Autoría

--- Main.java
+++ Main.java
@@ -5,47 +5,55 @@ import view.Table;
 import view.Screen;
 import view.Dialogs;
 
+/**
+ * 
+ * 
+ * Desarrollado por el equipo 4
+ * Héctor Jesús García Monroy
+ * Emiliano Hidalgo Gasca
+ * Gael González Méndez
+ */
 public class Main {
-    public static void main(String[] args) {
-        /**
-         * Ejecutable principal
-         * 
-         */
-        System.out.println("Hello world");
-        int propositions;
-        String expression;
-        Data data;
-        Table table;
-        Screen screen;
-        String[] headers = { "p", "q", "r", "s" };
-        String expressionMessage;
-        String joinedHeaders;
+        public static void main(String[] args) {
+                /**
+                 * Ejecutable principal
+                 * 
+                 */
+                System.out.println("Hello world");
+                int propositions;
+                String expression;
+                Data data;
+                Table table;
+                Screen screen;
+                String[] headers = { "p", "q", "r", "s" };
+                String expressionMessage;
+                String joinedHeaders;
 
-        propositions = Dialogs.getIntegerInRange(
-                "Número de proposiciones",
-                "Ingresa el número de proposiciones",
-                1, 4,
-                true);
+                propositions = Dialogs.getIntegerInRange(
+                                "Número de proposiciones",
+                                "Ingresa el número de proposiciones",
+                                1, 4,
+                                true);
 
-        headers = Arrays.copyOfRange(headers, 0, propositions);
-        joinedHeaders = String.join(", ", headers);
+                headers = Arrays.copyOfRange(headers, 0, propositions);
+                joinedHeaders = String.join(", ", headers);
 
-        expressionMessage = String.format("Escribe la operación lógica \nValores:%s\nOperadores: &&,||, !)",
-                joinedHeaders);
-        expression = Dialogs.getString("Expresión", expressionMessage);
+                expressionMessage = String.format("Escribe la operación lógica \nValores:%s\nOperadores: &&,||, !)",
+                                joinedHeaders);
+                expression = Dialogs.getString("Expresión", expressionMessage);
 
-        data = new Data(Arrays.copyOfRange(headers, 0, propositions), propositions);
-        data.addOperation(expression);
+                data = new Data(Arrays.copyOfRange(headers, 0, propositions), propositions);
+                data.addOperation(expression);
 
-        Dialogs.showDialog("Resultado",
-                String.format("La expresión ingresada es una %s", data.getLastPropositionType()));
+                Dialogs.showDialog("Resultado",
+                                String.format("La expresión ingresada es una %s", data.getLastPropositionType()));
 
-        screen = new Screen("Proposiciones - Equipo 7", 500, 400);
+                screen = new Screen("Proposiciones - Equipo 4", 500, 400);
 
-        table = new Table(data);
+                table = new Table(data);
 
-        screen.add(table);
-        screen.showScreen();
+                screen.add(table);
+                screen.showScreen();
 
-    }
+        }
 }
